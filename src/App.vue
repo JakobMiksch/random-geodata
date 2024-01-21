d0bcbbe33f86425a0ac1ed66bbcd417199d8f6a3
Jakob Miksch <info@jakobmiksch.eu>
2024-01-21 20:42:46 +0100

    Basic draw

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,53 @@
-<template>Hallo Welt</template>
+<template>
+    <OlMap :style="{ width: '500px', height: '500px' }" />
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { fromLonLat } from 'ol/proj'
+import { onMounted, ref } from 'vue'
+import { useOl, OlMap } from 'vue-ol-comp'
+import TileLayer from 'ol/layer/Tile'
+import OSM from 'ol/source/OSM'
+import Draw, {
+    createBox,
+} from 'ol/interaction/Draw'
+import VectorSource from 'ol/source/Vector'
+import VectorLayer from 'ol/layer/Vector'
+
+const chosenExtent = ref([])
+
+
+const { map } = useOl()
+
+onMounted(() => {
+    const drawSource = new VectorSource({ wrapX: false })
+    const draw = new Draw({
+        source: drawSource,
+        type: "Circle",
+        geometryFunction: createBox(),
+    });
+
+    draw.on(['drawstart', 'drawabort'], () => {
+        drawSource.clear()
+    })
+
+    map.value.addInteraction(draw);
+
+    const drawLayer = new VectorLayer({
+        source: drawSource
+    })
+
+    map.value.addLayer(
+        new TileLayer({
+            source: new OSM()
+        })
+    )
+
+    map.value.addLayer(drawLayer)
+
+    map.value.getView().setCenter(fromLonLat([11, 47]))
+    map.value.getView().setZoom(15)
+})
+</script>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './assets/main.css'
+import "ol/ol.css"
 
 import { createApp } from 'vue'
 import App from './App.vue'
