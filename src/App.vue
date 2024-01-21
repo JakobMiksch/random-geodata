2df00705f1c81cd5ec1049bdd102e251ceffb2a5
Jakob Miksch <info@jakobmiksch.eu>
2024-01-21 21:47:24 +0100

    Basic download

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
     <OlMap :style="{ width: '500px', height: '500px' }" />
     <div>Extent: {{ displayExtent }}</div>
-    <button @click="showPoints">Show points</button>
+    <button @click="downloadGeoJson">Download</button>
 </template>
 
 <script setup lang="ts">
 import { toLonLat, transformExtent, useGeographic } from 'ol/proj'
 import { computed, onMounted, ref, type Ref } from 'vue'
 import { useOl, OlMap } from 'vue-ol-comp'
-import {GeoJSON} from 'ol/format'
+import { GeoJSON } from 'ol/format'
 import TileLayer from 'ol/layer/Tile'
 import OSM from 'ol/source/OSM'
 import Draw, {
@@ -17,13 +17,14 @@ import Draw, {
 import VectorSource from 'ol/source/Vector'
 import VectorLayer from 'ol/layer/Vector'
 import type { Extent } from 'ol/extent'
-import {Feature as OlFeature} from "ol"
+import { Feature as OlFeature } from "ol"
 import { type Feature, type FeatureCollection, type GeoJsonProperties, type Point } from "geojson"
 useGeographic()
 
 const chosenExtent: Ref<Extent> = ref([])
 const displayExtent: Ref<Extent> = computed(() => chosenExtent.value.map((x) => parseFloat(x.toFixed(2))))
 
+const pointFeatureCollection = ref<FeatureCollection<Point, GeoJsonProperties>>()
 const pointSource = ref(new VectorSource({}))
 
 const createRandomPoints = (extent: Extent, count: number = 100): FeatureCollection<Point, GeoJsonProperties> => {
@@ -38,17 +39,30 @@ const createRandomPoints = (extent: Extent, count: number = 100): FeatureCollect
         const newY = Math.random() * (yMax - yMin) + yMin
         const coord3857 = [newX, newY]
         const coord4326 = toLonLat(coord3857)
-        const newFeature: Feature<Point> = {type: 'Feature', geometry: {type: 'Point', coordinates: coord4326}, properties: {}}
+        const newFeature: Feature<Point> = { type: 'Feature', geometry: { type: 'Point', coordinates: coord4326 }, properties: {} }
         result.features.push(newFeature)
     }
     return result
 }
 
 const showPoints = () => {
-    const points = createRandomPoints(chosenExtent.value)
-    const olFeatures = new GeoJSON().readFeatures(points) as OlFeature[]
+    pointFeatureCollection.value = createRandomPoints(chosenExtent.value)
+    const olFeatures = new GeoJSON().readFeatures(pointFeatureCollection.value) as OlFeature[]
     pointSource.value.addFeatures(olFeatures)
 }
+
+const downloadGeoJson = () => {
+  const jsonData = JSON.stringify(pointFeatureCollection.value, null, 2);
+  const blob = new Blob([jsonData], { type: 'application/json' });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = "random-geodata-" + displayExtent.value.join("-") + ".geojson";
+  document.body.appendChild(a);
+  a.click();
+  URL.revokeObjectURL(url);
+  document.body.removeChild(a);
+};
 
 const { map } = useOl()
 
